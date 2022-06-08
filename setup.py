3b48b95bfe8d548dcad5357885529cadc6e5ec3c
Viktor Akusov <viktor@akusov.xyz>
2022-06-08 17:08:41 +0300

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="michell-raytracer",
-    version="0.0.2",
+    version="0.0.3",
     author="Viktor Akusov",
     author_email="viktor@akusov.xyz",
     description="Relativistic non-linear raytracer for Schwarzschild's metric.",
